950bf69b5f908535a2e6f6236297560cab3a2454
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-08-20 22:47:16 +0300

    create test_instantiate_from_csv for class-method instantiate_from_csv

--- tests/test_item.py
+++ tests/test_item.py
@@ -67,3 +67,8 @@ def test_string_to_number():
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
     assert Item.string_to_number('5.5') == 5
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
