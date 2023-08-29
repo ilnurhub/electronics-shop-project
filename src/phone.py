57a87bf65352c83ff8dc5d8aadd885de3f8bd02d
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-08-29 21:28:10 +0300

    add def __init__()

--- src/phone.py
+++ src/phone.py
@@ -1,5 +1,11 @@
-from item import Item
+from src.item import Item
 
 
 class Phone(Item):
-    pass
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
+        super().__init__(name, price, quantity)
+        if not isinstance(number_of_sim, int):
+            raise ValueError('Значение должно быть числом.')
+        elif number_of_sim < 1:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        self.number_of_sim = number_of_sim
