17eca775d4348e0ada64eb71ed05cd21408bb26a
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-08-29 21:24:01 +0300

    create test_phone_number_of_sim

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -6,3 +6,9 @@ from src.phone import Phone
 def samsung():
     return Phone('samsung', 900, 10, 2)
 
+
+def test_phone_number_of_sim(samsung):
+    with pytest.raises(ValueError):
+        Phone('samsung', 900, 10, 1.55)
+        Phone('samsung', 900, 10, 0)
+    assert samsung.number_of_sim == 2
