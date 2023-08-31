2f922f4279858805f300d99ba4b9f483ec8b5c11
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-08-31 20:13:15 +0300

    add getter and setter for number_of_sim

--- src/phone.py
+++ src/phone.py
@@ -6,8 +6,6 @@ class Phone(Item):
         super().__init__(name, price, quantity)
         if not isinstance(number_of_sim, int):
             raise ValueError('Значение должно быть числом.')
-        elif number_of_sim < 1:
-            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
         self.number_of_sim = number_of_sim
 
     def __repr__(self):
@@ -16,3 +14,14 @@ class Phone(Item):
         Возвращает строку в формате "Название класса('название товара', цена товара, количество товара, количество симкарт)"
         """
         return super().__repr__()[0:-1] + f', {self.number_of_sim})'
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, number_of_sim):
+        if number_of_sim < 1:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        else:
+            self.__number_of_sim = number_of_sim
