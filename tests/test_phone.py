d254aaf10e0686c24804bb5e6dea8e87118645e1
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-08-31 20:53:28 +0300

    add test_add_quantity()

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,10 +1,16 @@
 import pytest
 from src.phone import Phone
+from src.item import Item
 
 
 @pytest.fixture
 def samsung():
     return Phone('samsung', 900, 10, 2)
+
+
+@pytest.fixture
+def iphone():
+    return Item('iphone', 1000, 5)
 
 
 def test_phone_number_of_sim(samsung):
@@ -25,3 +31,11 @@ def test_repr(samsung):
 def test_change_number_of_sim(samsung):
     with pytest.raises(ValueError):
         samsung.number_of_sim = 0
+
+
+def test_add_quantity(samsung, iphone):
+    assert iphone + samsung == 15
+    assert samsung + iphone == 15
+    assert samsung + samsung == 20
+    with pytest.raises(ValueError):
+        assert samsung + 100 == 110
