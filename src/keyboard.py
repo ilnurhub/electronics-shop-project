c9451173ae05d10a74bc5b4b306a2099d3126825
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-09-01 21:11:54 +0300

    add getter and setter for name

--- src/keyboard.py
+++ src/keyboard.py
@@ -4,3 +4,11 @@ from src.item import Item
 class Keyboard(Item):
     def __init__(self, name, price, quantity):
         super().__init__(name, price, quantity)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
