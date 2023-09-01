9e9e0ab361a5f686e46577f084b36bb286b88be1
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-09-01 21:53:07 +0300

    add getter for language

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,14 +3,18 @@ from src.item import Item
 
 class MixinLang:
     def __init__(self):
-        self.language = 'EN'
+        self.__language = 'EN'
 
     def change_lang(self):
-        if self.language == "EN":
-            self.language = "RU"
+        if self.__language == "EN":
+            self.__language = "RU"
         else:
-            self.language = "EN"
+            self.__language = "EN"
         return self
+
+    @property
+    def language(self):
+        return self.__language
 
 
 class Keyboard(Item, MixinLang):
