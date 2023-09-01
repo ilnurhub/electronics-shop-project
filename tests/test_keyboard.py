4aec6da94eb7b2eff186554e0ff6d72c26625f17
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-09-01 21:36:18 +0300

    add test_change_language()

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -13,3 +13,11 @@ def test_str(keyboard):
 
 def test_language(keyboard):
     assert str(keyboard.language) == 'EN'
+
+
+def test_change_language(keyboard):
+    keyboard.change_lang()
+    assert keyboard.language == "RU"
+
+    keyboard.change_lang().change_lang()
+    assert keyboard.language == "RU"
